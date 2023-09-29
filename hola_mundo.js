3affeadf2c094f3ebb5ab56309b00379d2f54744
Mario <malux97@hotmail.com>
2023-09-29 09:48:59 -0600

    Lectura con PHP

--- hola_mundo.js
+++ hola_mundo.js
@@ -6,5 +6,13 @@ console.log('Hola mundo desde Node.js, esto se verá desde la terminal de comand
 // Realizar una operación básica
 console.log(7*8);
 
-// Al usar una instrucción de JS para front en backend 
-console.log(window);
+// Al usar una instrucción de JS para front en backend, nos lanza un error en tiempo de ejecución
+// console.log(window);
+
+// Imprime en consola todo lo relacionado con el módulo GLOBAL
+console.log(global)
+
+// Funcion para imprimir cada segundo un mensaje
+setInterval(() => {
+    console.log('Hola NodeJS');
+}, 1000);
---
+++ leer.php
@@ -0,0 +1,13 @@
+<?php
+printf("Abriendo archivo");
+$archivo_url = fopen("archivo.txt","r");
+
+$texto = "";
+
+while ($fragmento = fgets($archivo_url)) {
+    $texto = $fragmento;
+}
+
+printf($texto);
+printf("<br>Haciendo otra cosa");
+?>

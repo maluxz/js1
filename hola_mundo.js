cc6f0d1b1725005744d85e252a1d24b4f04c9dc4
Mario <malux97@hotmail.com>
2023-09-29 09:27:09 -0600

    Instrucción error

--- hola_mundo.js
+++ hola_mundo.js
@@ -2,3 +2,9 @@
 
 // Imprimir en pantalla
 console.log('Hola mundo desde Node.js, esto se verá desde la terminal de comandos, ejecutado desde el servidor.');
+
+// Realizar una operación básica
+console.log(7*8);
+
+// Al usar una instrucción de JS para front en backend 
+console.log(window);

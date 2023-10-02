2c7f03e46f216f963eaf986110c25a726cc15d35
Mario <malux97@hotmail.com>
2023-10-02 12:56:05 -0600

    Agrega mejor lectura y benchmark

--- leer.php
+++ leer.php
@@ -1,13 +1,38 @@
 <?php
 printf("Abriendo archivo<br>");
-$archivo_url = fopen("archivo.txt","r");
 
-$texto = "";
+// Inicia el temporizador
+$start_time = microtime(true);
 
-while ($fragmento = fgets($archivo_url)) {
-    $texto = $fragmento;
+$archivo = 'archivo.txt';
+
+if (file_exists($archivo)) {
+    // Abrir el archivo en modo lectura
+    $handle = fopen($archivo, 'r');
+
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            // Mostrar cada línea leída
+            echo $line;
+        }
+
+        // Cerrar el archivo
+        fclose($handle);
+    } else {
+        echo "No se pudo abrir el archivo.";
+    }
+} else {
+    echo "El archivo no existe.";
 }
 
-printf($texto);
+// Detén el temporizador
+$end_time = microtime(true);
+
+// Calcula el tiempo transcurrido en segundos
+$execution_time = $end_time - $start_time;
+
+// Imprime el resultado
+echo "<br>Tiempo de ejecución: $execution_time segundos";
+
 printf("<br>Haciendo otra cosa");
 ?>

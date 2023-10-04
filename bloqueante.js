777184b56b735a34bc9a5c46f29d8f49a6831f38
Mario <malux97@hotmail.com>
2023-10-04 09:57:33 -0600

    UTF8

--- bloqueante.js
+++ bloqueante.js
@@ -1,8 +1,8 @@
-var fs = require("fs")
-console.log("Abriendo archivo")
+var fs = require("fs");
+console.log("Abriendo archivo");
 
-var content = fs.readFileSync("archivo.txt")
-console.log(content)
+var content = fs.readFileSync("archivo.txt","utf-8");
+console.log(content);
 
-console.log("\nHaciendo otra cosa")
-console.log(process.uptime())
+console.log("\nHaciendo otra cosa");
+console.log(process.uptime());

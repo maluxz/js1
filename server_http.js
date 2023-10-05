175a8fb4a0cc5647ee5b703b24fd23aaff17dcbf
Mario <malux97@hotmail.com>
2023-10-05 10:47:38 -0600

    Correción comillas concatenación y comentarios

--- server_http.js
+++ server_http.js
@@ -1,14 +1,24 @@
-// Llamamos al modulo http
+// Importamos el módulo http de Node.js
 const http = require('http');
-const hostname = '192.168.1.149';
+
+// Definimos la dirección IP y el puerto en el que escuchará el servidor
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req,res) => {
+// Creamos el servidor HTTP y especificamos una función de devolución de llamada
+const server = http.createServer((req, res) => {
+    // Configuramos el código de estado de la respuesta como 200 (OK)
     res.statusCode = 200;
-    res.setHeader ('Constent-Type', 'text/plain');
+
+    // Configuramos la cabecera (header) de la respuesta para indicar que el contenido es texto plano
+    res.setHeader('Content-Type', 'text/plain');
+
+    // Enviamos la respuesta al cliente con el mensaje "Hola mundo" y un salto de línea
     res.end('Hola mundo\n');
 });
 
-server.listen(port,hostname,() => {
-    console.log('Servidor corriendo en http://${hostname}:${port}/');
+// Hacemos que el servidor escuche en el puerto y la dirección IP especificados
+server.listen(port, hostname, () => {
+    // Cuando el servidor se inicia correctamente, mostramos un mensaje en la consola
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`);
 });

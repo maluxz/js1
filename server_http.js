29dd564f244ea05fd1aef84c6afc5eb87c6f89a9
Mario <malux97@hotmail.com>
2023-10-05 09:56:46 -0600

    Servidor completo funcionando

--- server_http.js
+++ server_http.js
@@ -1,1 +1,14 @@
 // Llamamos al modulo http
+const http = require('http');
+const hostname = '192.168.1.149';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader ('Constent-Type', 'text/plain');
+    res.end('Hola mundo\n');
+});
+
+server.listen(port,hostname,() => {
+    console.log('Servidor corriendo en http://${hostname}:${port}/');
+});
